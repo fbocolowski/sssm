85a9a0cfdec8b58f93fbbeb2d83d272cd7cd4794
fbocolowski <fbocolowski@gmail.com>
2019-09-09 14:14:57 -0300

    Changed puma log file

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -5,4 +5,4 @@ bind "unix:/srv/sssm/tmp/puma.sock"
 pidfile "/srv/sssm/tmp/puma.pid"
 state_path "/srv/sssm/tmp/puma.state"
 
-stdout_redirect "/srv/sssm/log/puma.log", "/srv/sssm/log/puma.log", true
+stdout_redirect "/srv/sssm/log/production.log", "/srv/sssm/log/production.log", true
