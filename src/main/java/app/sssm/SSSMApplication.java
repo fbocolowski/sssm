f6772fbc66783b9453107311df569badd64d9534
fbocolowski <fbocolowski@gmail.com>
2021-09-16 15:11:43 -0300

    Main class name changed Translated deploy command

--- src/main/java/app/sssm/SSSMApplication.java
+++ src/main/java/app/sssm/SSSMApplication.java
@@ -6,14 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SssmApplication {
+public class SSSMApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SssmApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SSSMApplication.class, args);
 //        Environment environment = context.getBean(Environment.class);
 
         RoleRepository roleRepository = context.getBean(RoleRepository.class);
